ed40b7873d762dab48584aae07f7b35c6098c947
kapil77Sharma <142788312+kapil77Sharma@users.noreply.github.com>
2023-11-27 21:29:22 +0530

    Update Assg2.py

--- Assg2.py
+++ Assg2.py
@@ -272,7 +272,7 @@ elif selected_page == "Model":
     import streamlit as st
     import random
     from graphviz import Digraph
-    graph = Digraph(graph_attr={'rankdir': 'LR'}, format='png', engine='dot', executable='/Users/kapilsharma/anaconda3/bin/dot')
+    graph = Digraph(graph_attr={'rankdir': 'LR'}, format='png', engine='dot', executable='requirements.txt')
 
 
     # Define a list of activation functions
